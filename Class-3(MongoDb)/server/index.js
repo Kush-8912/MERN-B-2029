9615feee763d770e715f938d479f4a96d3ad4646
mrinal1224 <iammrinalkc@gmail.com>
2026-08-18 15:03:40 +0530

    push

--- Class-3(MongoDb)/server/index.js
+++ Class-3(MongoDb)/server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+
+
 const dbUri =
   "mongodb+srv://mrinalbhattacharya_db_user:hOptSy87PqHkFLH7@cluster0.anop7ki.mongodb.net/LMS?appName=Cluster0";
 
@@ -15,9 +17,51 @@ mongoose
 
 const app = express();
 
+app.use(express.json())
+
+// Course Manamegmet Platform
+
+const courseSchema = new mongoose.Schema({
+  course_name: {
+    type: String,
+    required: true,
+  },
+
+  instructor: {
+    type: String,
+    required: true,
+  },
+
+  ratings: {
+    type: Number,
+  },
+
+  isPublished: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+let CourseModel = mongoose.model('course' , courseSchema)
+
 app.get("/", (req, res) => {
   res.send("Hello from the Server");
 });
+
+
+// Create a Course
+
+app.post('/api/courses' , async (req , res)=>{
+    await CourseModel.create({
+        course_name : req.body.course_name,
+        instructor : req.body.instructor,
+        ratings : req.body.ratings,
+        isPublished : req.body.isPublished
+      })
+
+
+      res.send('Course Created')
+})
 
 app.listen(8004, () => {
   console.log("Server Started");
