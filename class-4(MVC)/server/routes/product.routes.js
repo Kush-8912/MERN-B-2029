7af8cdb0afad494770f26bc0ef6a9a990d6ff430
mrinal1224 <iammrinalkc@gmail.com>
2026-08-25 14:33:25 +0530

    MVC

--- class-4(MVC)/server/routes/product.routes.js
+++ class-4(MVC)/server/routes/product.routes.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const ProductModel = require('../models/products.model.js')
-const {createProduct} = require('../controllers/product.controllers.js')
+const {createProduct , getAllProducts , updateProduct , deleteProduct} = require('../controllers/product.controllers.js')
 
 
 const productRoutes = express.Router() // This helps you initilaie routing
 
 
-// productRoutes.get()
+productRoutes.get('/getAll' ,getAllProducts )
 
 productRoutes.post('/create' ,createProduct )
 
-// productRoutes.put()
+productRoutes.put('/update/:id' , updateProduct)
 
-// productRoutes.delete()
+ productRoutes.delete('/delete/:id' , deleteProduct)
 
 
 module.exports = productRoutes
